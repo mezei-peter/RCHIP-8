963a77cb63bb2ac5a890d9867a35bce0bf7230e5
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-12 18:07:30 +0200

    Add Memory and Cpu details

--- src/guestsystem/components/cpu.rs
+++ src/guestsystem/components/cpu.rs
@@ -1,3 +1,21 @@
-pub struct Cpu {
+const VARIABLE_REGISTER_COUNT: usize = 16;
 
+pub struct Cpu {
+    program_counter: u16,
+    index_register: u16,
+    variable_registers: [u16; VARIABLE_REGISTER_COUNT],
+    delay_timer: u16,
+    sound_timer: u16,
+}
+
+impl Cpu {
+    pub fn new() -> Cpu {
+        Cpu {
+            program_counter: 0,
+            index_register: 0,
+            variable_registers: [0; VARIABLE_REGISTER_COUNT],
+            delay_timer: 0,
+            sound_timer: 0,
+        }
+    }
 }
--- src/guestsystem/components/memory.rs
+++ src/guestsystem/components/memory.rs
@@ -1,1 +1,15 @@
-pub struct Memory;
+const FOUR_KIBI: usize = 4096;
+const START_ADDRESS: u16 = 0x200;
+
+pub struct Memory {
+    heap: [u8; FOUR_KIBI],
+    stack: Vec<u16>,
+}
+
+impl Memory {
+    pub fn new() -> Memory {
+        Memory { heap: [0; FOUR_KIBI], stack: Vec::new() }
+    }
+}
+
+
