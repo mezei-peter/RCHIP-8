95abfefe0ec81ff7aaecc6640838eb67f5fbb47b
mezeipb <mezei.peter.barnabas@gmail.com>
2023-05-12 21:14:32 +0200

    Initialize window

--- src/guestsystem/components/display.rs
+++ src/guestsystem/components/display.rs
@@ -1,7 +1,19 @@
-pub struct DisplayScreen;
+use sdl2::{Sdl, VideoSubsystem, video::Window, render::Canvas};
+
+pub struct DisplayScreen {
+    context: Sdl,
+    canvas: Canvas<Window>,
+}
 
 impl DisplayScreen {
     pub fn new() -> DisplayScreen {
-        DisplayScreen
+        let context: Sdl = sdl2::init().unwrap();
+        let video_subsystem: VideoSubsystem = context.video().unwrap();
+        let window: Window = video_subsystem.window("RCHIP-8", 800, 600)
+            .position_centered()
+            .build()
+            .unwrap();
+        let mut canvas: Canvas<Window> = window.into_canvas().build().unwrap();
+        DisplayScreen{context, canvas}
     }
 }
